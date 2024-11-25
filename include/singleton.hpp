a21f11452aaa6814a789fd0753bddf29cf6227a3
wtfy <123456@email.com>
2024-11-25 18:35:08 +0800

    init project and add singleton

---
+++ include/singleton.hpp
@@ -0,0 +1,43 @@
+#ifndef _SINGLETON_HPP_
+#define _SINGLETON_HPP_
+
+
+template <typename T>
+class Singleton{
+    public:
+        static inline T* getInstance();
+        void release();
+
+        Singleton(Singleton const&) = delete;
+        Singleton(Singleton&&) = delete;
+        Singleton& operator=(Singleton const&) = delete;
+        Singleton& operator=(Singleton&&) = delete;
+
+    protected:
+        Singleton() {};
+        ~Singleton() {};
+        static T* _instance;
+};
+
+
+template <typename T>
+inline T* Singleton<T>::getInstance(){
+    if (!_instance) {
+        _instance = new T;
+    }
+    return _instance;
+}
+
+
+template <typename T>
+void Singleton<T>::release(){
+    if (_instance) {
+        delete _instance;
+    }
+    _instance = nullptr;
+    return;
+}
+
+
+
+#endif
